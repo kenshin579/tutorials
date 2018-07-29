28798af0076c3a3acaa009757a353e02ad0e0217
ykoh <kenshin579@gmail.com>
2018-07-29 17:46:12 +0900

    chapter 3: Routing - Introducing the vue-router   ㅁ. /thread/:id router 추가

---
+++ vueshool.io/master_class/03_Routing/vueschool-forum/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+@import "assets/css/style.css";
+
+</style>
