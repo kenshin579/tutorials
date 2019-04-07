d6258858dec2b2e12a55c55a7eeb12d74d338a5b
Frank <frankyoh@tmon.co.kr>
2019-04-07 21:58:29 +0900

    studying reactjs tutorial

---
+++ velopert/reactjs/hello-react/src/App.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MyName from './MyName';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                    <div>
+                        {
+                            1 + 1 === 2 ? (<div>맞아요!</div>) : (<div>틀려요!</div>)
+                        }
+                    </div>
+                    <MyName name="프랭크" />
+                </header>
+            </div>
+        );
+    }
+}
+
+export default App;
