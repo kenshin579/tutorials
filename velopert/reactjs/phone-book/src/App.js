51db13a3db03a48037fb51221dacc68b7b2dc782
Frank <frankyoh@tmon.co.kr>
2019-04-08 22:03:40 +0900

    데이터 필더링

--- velopert/reactjs/phone-book/src/App.js
+++ velopert/reactjs/phone-book/src/App.js
@@ -18,7 +18,14 @@ class App extends Component {
                 name: '홍길동',
                 phone: '010-0000-0001'
             }
-        ]
+        ],
+        keyword: ''
+    };
+
+    handleChange = (e) => {
+        this.setState({
+            keyword: e.target.value,
+        });
     };
 
     handleCreate = (data) => {
@@ -48,13 +55,26 @@ class App extends Component {
     };
 
     render() {
-        const {information} = this.state;
+        const {information, keyword} = this.state;
+        const filteredList = information.filter(
+            info => info.name.indexOf(keyword) !== -1
+        );
 
         return (
             <div>
-                <PhoneForm onCreate={this.handleCreate}/>
+                <PhoneForm
+                    onCreate={this.handleCreate}
+                />
+                <p>
+                    <input
+                        placeholder="검색 할 이름을 입력하세요.."
+                        onChange={this.handleChange}
+                        value={keyword}
+                    />
+                </p>
+                <hr/>
                 <PhoneInfoList
-                    data={this.state.information}
+                    data={filteredList}
                     onRemove={this.handleRemove}
                     onUpdate={this.handleUpdate} //handleUpdate 함수는 PhoneInfoList에 전달됨
                 />
--- velopert/reactjs/phone-book/src/components/PhoneInfoList.js
+++ velopert/reactjs/phone-book/src/components/PhoneInfoList.js
@@ -8,7 +8,14 @@ class PhoneInfoList extends Component {
         onUpdate: () => console.warn('onUpdate not defined')
     };
 
+    //이 함수가 없으면 App가 데이터가 변경될 때마다 아래 자식 모두 render이
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.data !== this.props.data; //다음 받아올 data가 현재 data랑 다른 배열일 때 true로 설정하게 하면 됨
+    }
+
     render() {
+        console.log('render PhoneInfoList');
+
         const {data, onRemove, onUpdate} = this.props; //부모(App) 데이터 접근
         const list = data.map(
             info => (
