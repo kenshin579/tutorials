ac142500349d80989b6e68fd53d9d83b54a30412
frankyoh <frankyoh@tmon.co.kr>
2020-08-30 22:35:58 +0900

    [TUT-39] momentum basic react 예제

---
+++ chrome-extension/momentum-basic/src/Components/GlobalStyle.js
@@ -0,0 +1,27 @@
+import {createGlobalStyle} from 'styled-components';
+import reset from 'styled-reset'; // css reset
+
+const GlobalStyle = createGlobalStyle`
+    ${reset};
+    a{
+        text-decoration: none;
+        color:inherit;
+    }
+    *{
+        box-sizing:border-box;
+    }
+    body{
+        font-family: 'Nanum Gothic', sans-serif;
+        font-size: 1rem;
+		color:#fff;
+        width:100%;
+        height:100vh;
+        display:flex;
+        justify-content:center;
+        align-items:center;
+        background: url(https://source.unsplash.com/7vKP5BAm8wg/1600x900);
+        background-size:cover;
+    }
+`;
+
+export default GlobalStyle;
